18f7797d47e57e177b127e07c7a4efe5918ca8ba
sebug <sebastian.gfeller@gmail.com>
2020-03-07 16:12:19 +0100

    Preparing for POST

--- index.js
+++ index.js
@@ -7,15 +7,24 @@ if (process.argv.length < 6) {
 }
 
 const loginUrl = process.argv[2];
+const finalUrl = process.argv[3];
+const userName = process.argv[4];
+const password = process.argv[5];
 
 class CustomResourceLoader extends jsdom.ResourceLoader {
     fetch(url, options) {
-	console.log(url);
+	if (this.startLogging) {
+	    console.log(url);
+	}
 	return super.fetch(url, options);
     }
 }
 const resourceLoader = new CustomResourceLoader();
 const cookieJar = new jsdom.CookieJar();
+
+const requestString = `username=${userName}&password=${password}&grant_type=password`
+
+console.log(requestString);
 
 JSDOM.fromURL(loginUrl, {
     runScripts: 'dangerously',
@@ -23,7 +32,9 @@ JSDOM.fromURL(loginUrl, {
     resources: resourceLoader,
     cookieJar: cookieJar
 }).then(dom => {
+    
     console.log('Loaded');
+    console.log(dom.serialize());
 });
 
 
